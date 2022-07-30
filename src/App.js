5a40e0f78a8e3b775e6e799bc5ba6d5d4000881a
Ahmed AL Awamleh <awamlehad@icloud.com>
2022-07-30 14:26:12 +0300

    1+2+3

--- src/App.js
+++ src/App.js
@@ -21,31 +21,18 @@ class App extends Component {
   applyPickedLanguage = (pickedLanguage, oppositeLangIconId) => {
     this.swapCurrentlyActiveLanguage(oppositeLangIconId);
     document.documentElement.lang = pickedLanguage;
-    var resumePath =
-      document.documentElement.lang === window.$primaryLanguage
-        ? `res_primaryLanguage.json`
-        : `res_secondaryLanguage.json`;
+    var resumePath = `res_primaryLanguage.json`;
     this.loadResumeFromPath(resumePath);
   }
 
   swapCurrentlyActiveLanguage = (oppositeLangIconId) => {
-    var pickedLangIconId =
-      oppositeLangIconId === window.$primaryLanguageIconId
-        ? window.$secondaryLanguageIconId
-        : window.$primaryLanguageIconId;
-    document
-      .getElementById(oppositeLangIconId)
-      .removeAttribute("filter", "brightness(40%)");
-    document
-      .getElementById(pickedLangIconId)
-      .setAttribute("filter", "brightness(40%)");
+
   }
 
   componentDidMount = () => {
     this.loadSharedData();
     this.applyPickedLanguage(
-      window.$primaryLanguage,
-      window.$secondaryLanguageIconId
+      window.$primaryLanguage
     );
   }
 
@@ -82,28 +69,32 @@ class App extends Component {
       <Router>
         <Header sharedData={this.state.sharedData.basic_info} />
         <Routes>
-          <Route 
-            exact path="/" 
+          <Route
+            exact path="/"
             element={<Home
               resumeData={this.state.resumeData}
               sharedData={this.state.sharedData}
             />}>
           </Route>
-          <Route 
-            path="/about" 
+          <Route
+            path="/about"
             element={<About
               resumeBasicInfo={this.state.resumeData.basic_info}
               sharedBasicInfo={this.state.sharedData.basic_info}
             />}>
           </Route>
         </Routes>
-        <Footer 
+     
+        <Footer
           sharedBasicInfo={this.state.sharedData.basic_info}
-          applyPickedLanguage={this.applyPickedLanguage} 
-        />  
+          applyPickedLanguage={this.applyPickedLanguage}
+        />
       </Router>
     );
   }
 }
 
 export default App;
+/*
+ 
+      */
